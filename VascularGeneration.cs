612ede13d4b412e70cdcf21cdef34af1355f18b4
Chase Lowney <chasea2016@gmail.com>
2025-10-30 17:37:17 -0400

    setting up c# stuff

---
+++ VascularGeneration.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class VascularGeneration
+{
+
+    //fields
+    
+    //constructor
+    public VascularGeneration()
+    {
+        Console.WriteLine("hello world");
+    }
+
+    static void Main()
+    {
+        VascularGeneration testing = new VascularGeneration();
+    }
+
+}
